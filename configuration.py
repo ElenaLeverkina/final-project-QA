e55521d5e566e646ab2cc8b99dac30214dc69e24
ElenaLeverkina <leverkina.e@yandex.ru>
2024-01-20 16:44:28 +0500

    Добавила файлы

---
+++ configuration.py
@@ -0,0 +1,2 @@
+URL_SERVICE =  "https://924ff7a1-78c1-4ee3-90d4-148b3744ef1c.serverhub.praktikum-services.ru"
+CREAT_ORDERS = "/api/v1/orders"
---
+++ main.py
@@ -0,0 +1,7 @@
+import configuration
+import requests
+import data
+import sender_stand_request
+
+sender_stand_request.test_order_creation_and_retrieval()
+
